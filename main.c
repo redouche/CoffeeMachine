0999fa7d7407d026352c07e208a0bbc65192f789
reda <itsrazenyt@gmail.com>
2025-02-13 10:53:44 +0100

    created a simple menu

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+
+int menu() {
+    int choix = 0;
+    printf("Bonjour ! Quel type de café souhaitez vous ?");
+    printf("1: Expresso");
+    printf("2: Capuccino");
+    printf("3: Macciato");
+    scanf("%d", &choix);
+}
+
 int main(void) {
     Coffee Expresso = {"Reda", 10, 2, true, false, true};
     return 0;
