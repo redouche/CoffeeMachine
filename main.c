ffa68082253c27730a239a5de43db11c32441e9a
reda <itsrazenyt@gmail.com>
2025-02-13 11:34:10 +0100

    better / harder version, to show the same result, dynamic allocation for the Cup choice

--- main.c
+++ main.c
@@ -9,12 +9,13 @@ int menu() {
     int choice = 0;
     Coffee* myCup = NULL;
 
-    printf("Bonjour ! Quel type de cafe souhaitez vous ?\n");
+    printf("Serveur - Bonjour ! Quel type de cafe souhaitez vous ?\n");
     printf("1: Expresso\n");
     printf("2: Capuccino\n");
     printf("3: Macciato\n");
+    printf("Votre choix :");
     scanf("%d", &choice);
-    myCup = CoffeeMachine(choice);
+    CoffeeMachine(choice, &myCup);
     showCup(*myCup);
     if (myCup != NULL) {
         free(myCup);
@@ -23,25 +24,24 @@ int menu() {
 
 }
 
-Coffee* CoffeeMachine(int client_choice) {
-    Coffee * Cup = NULL;
-    Cup = malloc(sizeof(Coffee));
+void CoffeeMachine(int client_choice, Coffee** myCup) {
+    *myCup = calloc(1, sizeof(Coffee));
     switch (client_choice) {
         case EXPRESSO:
-            strcpy(Cup->name, "Expresso");
-            Cup->volume = SMALL;
-            Cup->price = 1.0f;
-            Cup->milk = false;
-            Cup->chocolate = false;
-            Cup->sugar = true;
+            strcpy((*myCup)->name, "Expresso");
+            (*myCup)->volume = SMALL;
+            (*myCup)->price = 1.0f;
+            (*myCup)->milk = false;
+            (*myCup)->chocolate = false;
+            (*myCup)->sugar = true;
             break;
         case CAPUCCINO:
-            strcpy(Cup->name, "Capuccino");
-            Cup->volume = SMALL;
-            Cup->price = 2.0f;
-            Cup->milk = true;
-            Cup->chocolate = false;
-            Cup->sugar = true;
+            strcpy((*myCup)->name, "Capuccino");
+            (*myCup)->volume = SMALL;
+            (*myCup)->price = 2.0f;
+            (*myCup)->milk = true;
+            (*myCup)->chocolate = false;
+            (*myCup)->sugar = true;
             break;
         case MACCIATO:
             break;
@@ -49,11 +49,10 @@ Coffee* CoffeeMachine(int client_choice) {
             printf("Nous n'avons pas cette article !");
             break;
     }
-    return Cup;
 }
 
 void showCup(Coffee Cup) {
-    printf("Hmmm, un cafe %s qui coute %.2f euros", Cup.name, Cup.price);
+    printf("Vous - Hmmm, un cafe %s qui coute %.2f euros", Cup.name, Cup.price);
 }
 
 int main(void) {
